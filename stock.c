aa8c0ae3fb07bb1a939531c0488ea6e24f38b0b1
LifeGo <13142436@qq.com>
2016-03-29 21:41:53 +0800

    add stock backtest code.

---
+++ stock.c
@@ -0,0 +1,698 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "memory.h"
+
+#include "stock.h"
+
+#define MAX_LINE_LENGTH					(8*1024)
+
+static mem_root groot;
+
+int load_index(char *filename)
+{
+	int ret = 0;
+	int len = 0;
+	int size = 0;
+	int first = 0;
+	char *cpwork;
+	char *cpend;
+	FILE *fin;
+
+	char *file_buf = NULL;
+	char *name_buf = NULL;
+	char **name = groot.indexfile->name;
+	int  name_cnt  = 0;
+
+	fin = fopen((char *)filename, "rb");
+	if (fin)
+	{
+		fseek( fin, 0L, SEEK_END);
+		len = ftell( fin );
+		size = len + 128;
+
+		file_buf = (char *)malloc( (size_t)(size) );
+		if (!file_buf)
+		{
+			printf("memory not enough memory.\n");
+			fclose(fin);
+			return -1;
+		}
+		memset((void *)file_buf, 0x00, size);
+
+		fseek(fin, 0L, SEEK_SET);
+		ret = fread(file_buf, len, 1, fin);
+		if (1 != ret) return -2;
+		fclose(fin);
+
+		cpwork = file_buf;
+		cpend  = file_buf + len;
+		*(cpend+0) = 0x0D;
+		*(cpend+1) = 0x0A;
+
+		first = 1;
+		while (cpwork < cpend)
+		{
+			if ( (*cpwork != ' ') && (*cpwork != '\t') && (*cpwork != 0x0D) && (*cpwork != 0x0A) )
+			{
+				if (first == 1)
+				{
+					first = 0;
+					*name++ = cpwork;
+					name_cnt++;
+				}
+			}
+			else
+			{
+				*cpwork = '\0';
+				first = 1;
+			}
+			cpwork++;
+		}
+	}
+
+	if (name_cnt > 0)
+	{
+		groot.indexfile->used = name_cnt;
+		groot.indexfile->buffer = file_buf;
+	}
+
+	return 0;
+}
+
+//"1997-07-04" to "19970704"
+#define DEFAULT_DATE            (19770812)
+int str_to_date(char *istr)
+{
+	int idx = 0;
+	int idate = DEFAULT_DATE;
+	char date[12];
+	char *cp = istr;
+
+	if (cp == NULL)
+		return DEFAULT_DATE;
+
+	while (*cp != '\0')
+	{
+		if ( (*cp >= '0') && (*cp <= '9') && (*cp != '-') )
+			date[idx++] = *cp;
+		else if (*cp != '-')
+		{
+			printf("**<%s>**\n", istr);
+			return DEFAULT_DATE;
+		}
+
+		cp++;
+	}
+
+	date[idx] = '\0';
+
+	return atoi(date);
+}
+
+int str_to_stockid(char *istr)
+{
+	int idx = 0;
+	char date[12];
+	char *cp = istr;
+
+	if (cp == NULL)
+		return 0;
+
+	while (*cp != '\0')
+	{
+		if      (*cp == 'S') { date[idx++] = '8'; cp++; }
+		else if (*cp == 'H') { date[idx++] = '6'; cp++; }
+		else if (*cp == 'Z') { date[idx++] = '8'; cp++; }
+		else if ( ((*cp >= '0') && (*cp <= '9')) )
+			date[idx++] = *cp++;
+		else
+			return 0;
+	}
+
+	date[idx] = '\0';
+	return atoi(date);
+}
+
+int parse_line(char *ibuf, char *ocode, char *oname, day_data *oitem)
+{
+	int cnt = 0;
+	int first = 0;
+	char *cp= ibuf;
+	char *item[128];
+
+	if ( (ibuf == NULL) || (ocode == NULL) || (oname == NULL) || (oitem == NULL) )
+		return -1;
+
+	while (*cp != '\0')
+	{
+		if( (*cp == '@') || (*cp == '#') || (*cp == '$') )
+			return 0;
+
+		if ( (*cp == '\t') || (*cp == ' ') || (*cp == ',') )
+		{
+			*cp++ = '\0';;
+			first = 1;
+			continue;
+		}
+		if (first == 1)
+		{
+			first = 0;
+			item[cnt++] = cp;
+		}
+		cp++;
+	}
+
+	strcpy(ocode, item[0]);
+	strcpy(oname, item[1]);
+
+	oitem->date 	= str_to_date(item[2]);
+	oitem->price.open 	= atof(item[3 ]);
+	oitem->price.close 	= atof(item[4 ]);
+	oitem->price.high 	= atof(item[5 ]);
+	oitem->price.low 	= atof(item[6 ]);
+	oitem->mama.ma5 	= atof(item[7 ]);
+	oitem->mama.ma10 	= atof(item[8 ]);
+	oitem->mama.ma13 	= atof(item[9 ]);
+	oitem->mama.ma21 	= atof(item[10]);
+	oitem->mama.ma34 	= atof(item[11]);
+	oitem->mama.ma55 	= atof(item[12]);
+	oitem->mama.ma89 	= atof(item[13]);
+	oitem->mama.ma144 	= atof(item[14]);
+	oitem->mama.ma233 	= atof(item[15]);
+	oitem->macd.macd 	= atof(item[16]);
+	oitem->macd.diff 	= atof(item[17]);
+	oitem->macd.dea 	= atof(item[18]);
+	oitem->boll.upr 	= atof(item[19]);
+	oitem->boll.mid 	= atof(item[20]);
+	oitem->boll.dwn 	= atof(item[21]);
+	oitem->cci.cci14 	= atof(item[22]);
+	oitem->cci.cci21 	= atof(item[23]);
+	oitem->cci.cci55 	= atof(item[24]);
+
+	return cnt;
+}
+
+int load_csv(char *filename)
+{
+	int ret = 0;
+	int cnt = 0;
+	int idx = 0;
+	int filelen = 0;
+	int linelen = 0;
+	int memsize = 0;
+	char *cpmem;
+	char *cpwork;
+	char *cpend;
+	FILE *fin;
+
+	char sname[16];
+	char scode[16];
+	int  day_cnt = 0;
+	day_data daydata;
+	stock_data *stock;
+	stock_data *new_mem;
+
+	char line_buf[MAX_LINE_LENGTH];
+	char *item[MAX_LINE_LENGTH];
+
+	fin = fopen((char *)filename, "rb");
+	if (fin)
+	{
+		fseek(fin, 0L, SEEK_END);
+		filelen = ftell( fin );
+		memsize = 2 * filelen + 128;
+
+		cpmem = (char *)malloc( (size_t) memsize );
+		if (cpmem == NULL)
+		{
+			printf("memory not enough memory.\n");
+			fclose(fin);
+			return -1;
+		}
+		memset((void *)cpmem, 0x00, memsize);
+		cpwork = cpmem;
+		cpend  = cpmem + filelen;
+		stock  = (stock_data*) (cpend + 64);
+		*(cpend+0) = 0x0D;
+		*(cpend+1) = 0x0A;
+
+		fseek(fin, 0L, SEEK_SET);
+		ret = fread(cpmem, filelen, 1, fin);
+		if (1 != ret) return -2;
+		fclose(fin);
+
+		linelen = 0;
+		while (cpwork < cpend)
+		{
+			if ( (*cpwork != 0x0D) && (*cpwork != 0x0A) )
+			{
+				if (linelen >= MAX_LINE_LENGTH-2)
+				{
+					line_buf[linelen++] = '\0';
+					line_buf[linelen  ] = '\0';
+					printf("[%4d]:<%s>\n", linelen, line_buf);
+					linelen = 0;
+				}
+				line_buf[linelen++] = *cpwork++;
+			}
+			else
+			{
+				line_buf[linelen++] = '\0';
+				line_buf[linelen  ] = '\0';
+				cnt = parse_line(line_buf, scode, sname, &daydata);
+				if (cnt > 0)
+				{
+					if (stock->code[0] == '\0') strcpy(stock->code, scode);
+					if (stock->name[0] == '\0') strcpy(stock->name, sname);
+					memcpy(&stock->data[day_cnt], &daydata, sizeof(day_data));
+					day_cnt++;
+				}
+
+				linelen = 0;
+				while ((*cpwork == 0x0D) || (*cpwork == 0x0A) ) cpwork++;
+			}
+		}
+
+		if (day_cnt > 0)
+		{
+			memsize = (~0x0F) & (15 + sizeof(stock_data) + day_cnt*sizeof(day_data));
+			new_mem = (stock_data*)malloc( (size_t)(memsize) );
+			if (new_mem) 
+			{
+				stock->stockid = str_to_stockid(stock->code);
+				stock->records = day_cnt;
+				stock->memsize = memsize;
+				memcpy(new_mem, stock, memsize);
+
+				groot.stockmap->stocks[groot.stockmap->used++] = new_mem;
+			}
+		}
+
+		free(cpmem);
+	}
+
+	return 0;
+}
+
+int init_mem()
+{
+	void *vp = NULL;
+	memset(&groot, 0x00, sizeof(mem_root));
+
+	vp = malloc(sizeof(index_file));
+	if (vp) {
+		memset(vp, 0x00, sizeof(index_file));
+		groot.indexfile = vp;
+	}
+
+	vp = malloc(sizeof(trade_date));
+	if (vp) {
+		memset(vp, 0x00, sizeof(trade_date));
+		groot.tradedate = vp;
+	}
+
+	vp = malloc(sizeof(stock_map));
+	if (vp) {
+		memset(vp, 0x00, sizeof(stock_map));
+		groot.stockmap = vp;
+	}
+
+	return 0;
+}
+
+int free_mem()
+{
+	int idx = 0;
+
+	for (idx=0; idx<groot.stockmap->used; idx++)
+		free(groot.stockmap->stocks[idx]);
+
+	if (groot.indexfile->buffer)
+		free (groot.indexfile->buffer);
+
+	free(groot.tradedate);
+	free(groot.indexfile);
+	free(groot.stockmap );
+
+	memset(&groot, 0x00, sizeof(mem_root));
+
+	return 0;
+}
+
+int int_sort(int *array, int len)
+{
+	int idx = 0;
+	int beg = 0;
+	int end = len-1;
+
+	while ( beg < end )
+	{
+		int minid = beg;
+		int maxid = end;
+		int value = 0;
+
+		value = array[beg];
+		if (value > array[end])
+		{
+			array[beg] = array[end];
+			array[end] = value;
+		}
+
+		for (idx=beg; idx<=end; idx++)
+		{
+			value = array[idx];
+			if ( value > array[maxid]) maxid = idx;
+			if ( value < array[minid]) minid = idx;
+		}
+
+		value = array[maxid];
+		array[maxid] = array[end];
+		array[end] = value;
+
+		value = array[minid];
+		array[minid] = array[beg];
+		array[beg] = value;
+
+		beg++; end--;
+	}
+
+	return 0;
+}
+
+int get_tradeday_list()
+{
+	trade_date *tdate = groot.tradedate;
+
+	printf("tdate->used= %d\n", tdate->used);
+	for (int idx=0; idx<groot.stockmap->used; idx++)
+	{
+		stock_data *stock = (stock_data *)groot.stockmap->stocks[idx];
+
+		for (int idy=0; idy<stock->records; idy++)
+		{
+			int find  = 0;
+			int today = stock->data[idy].date;
+			for (int idz=0; idz<tdate->used; idz++)
+			{
+				if (today == tdate->date[idz])
+				{
+					find = 1;
+					break;
+				}
+			}
+			if (0 == find)
+			{
+				tdate->date[tdate->used++] = today;
+			}
+		}
+	}
+
+	int_sort(tdate->date, tdate->used);
+
+	int *day1 = tdate->date;
+	int *day2 = day1 + 1;
+	for (int idx=0; idx<tdate->used-1; idx++)
+	{
+		printf("%d\n", *day1);
+		if ( *day1 >= *day2 )
+			printf("ERROR: idx = %d\n", idx);
+		day1++;
+		day2++;
+	}
+	printf("tdate->used= %d\n", tdate->used);
+
+	return 1;
+}
+
+int mem_dump(char *file)
+{
+	FILE *fpout;
+	section_block section;
+
+	fpout = fopen((char *)file, "wb");
+	if (fpout)
+	{
+		/* head */
+		memset(&section, 0x00, sizeof(section_block));
+		sprintf(section.name, ".section=<%s>", "head");
+		section.size = 0;
+		fwrite(&section, sizeof(section_block), 1, fpout);
+
+		/* trade_day */
+		memset(&section, 0x00, sizeof(section_block));
+		sprintf(section.name, ".section=<%s>", "tradedate");
+		section.size = sizeof(trade_date);
+		fwrite(&section, sizeof(section_block), 1, fpout);
+		fwrite(groot.tradedate, section.size, 1, fpout);
+
+		/* stock_cnt */
+		memset(&section, 0x00, sizeof(section_block));
+		sprintf(section.name, ".section=<%s>", "stockmap");
+		section.size = groot.stockmap->used;
+		fwrite(&section, sizeof(section_block), 1, fpout);
+
+		/* stock_data */
+		for (int idx=0; idx<groot.stockmap->used; idx++)
+		{
+			stock_data *stock = (stock_data *)groot.stockmap->stocks[idx];
+
+			memset(&section, 0x00, sizeof(section_block));
+			sprintf(section.name, ".stock<%s>", stock->code);
+			section.size = stock->memsize;
+			fwrite(&section, sizeof(section_block), 1, fpout);
+			fwrite(stock, section.size, 1, fpout);
+		}
+
+		fclose(fpout);
+		return 0;
+	}
+
+	return -1;
+}
+
+int mem_load(char *file)
+{
+	int ret = 0;
+	int size = 0;
+	int count = 0;
+	FILE *fpin;
+	stock_data *stock;
+	section_block section;
+
+	fpin = fopen((char *)file, "rb");
+	if (fpin)
+	{
+		/* head */
+		memset(&section, 0x00, sizeof(section_block));
+		fread(&section, sizeof(section_block), 1, fpin);
+		if ( (strstr(section.name, "head") == NULL) || (section.size != 0) )
+		{
+			ret = -1;
+			goto _error;
+		}
+
+		/* trade_date */
+		memset(&section, 0x00, sizeof(section_block));
+		fread(&section, sizeof(section_block), 1, fpin);
+		if ( (strstr(section.name, "tradedate") == NULL)
+				|| (section.size != sizeof(trade_date)) )
+		{
+			ret = -2;
+			goto _error;
+		}
+		fread(groot.tradedate, section.size, 1, fpin);
+
+		/* stock_cnt */
+		memset(&section, 0x00, sizeof(section_block));
+		fread(&section, sizeof(section_block), 1, fpin);
+		if (strstr(section.name, "stockmap") == NULL)
+		{
+			ret = -3;
+			goto _error;
+		}
+		count = section.size;
+		printf("load %d stocks data to memory.\n", count);
+
+		while ( !feof(fpin) )
+		{
+			memset(&section, 0x00, sizeof(section_block));
+			fread(&section, sizeof(section_block), 1, fpin);
+			if (strstr(section.name, ".stock") == NULL)
+			{
+				ret = -4;
+				goto _error;
+			}
+
+			/* stock_data */
+			stock_data *sd = malloc( (size_t)(section.size) );
+			if (!sd)
+			{
+				ret = -5;
+				goto _error;
+			}
+
+			fread(sd, section.size, 1, fpin);
+#if 0 // for Debug
+			printf("code:<%s> stockid=%d records=%d\n",
+					sd->code, sd->stockid, sd->records);
+#endif
+			groot.stockmap->stocks[groot.stockmap->used++] = sd;
+			count--;
+		}
+		if (count) ret = -5;
+	}
+_error:
+	fclose(fpin);
+	return ret;
+}
+
+int back_test_go(backtest *bt)
+{
+	int idx   = 0;
+	int idy   = 0;
+	int beg   = 20100101;
+	int end   = 20101231;
+	int days  = 0;
+	stock_data *stock;
+
+	if (bt == NULL) return -1;
+
+	beg = bt->beg;
+	end = bt->end;
+
+	int *today = groot.tradedate->date;
+	while (*today < beg) today++;
+
+	while ( (*today) && (*today <= end) )
+	{
+		for (int idx=0; idx<groot.stockmap->used; idx++)
+		{
+			stock_data *stock = (stock_data *)groot.stockmap->stocks[idx];
+			for (int idy=0; idy<stock->records; idy++)
+			{
+				int m_date = stock->data[idy].date;
+				if (m_date  == *today)
+				{
+					printf("[%d]: %d open:%6.02f, close:%6.02f, high:%6.02f, low:%6.02f\n",
+							stock->stockid,
+							stock->data[idy].date,
+							stock->data[idy].price.open,
+							stock->data[idy].price.close,
+							stock->data[idy].price.high,
+							stock->data[idy].price.low);
+					break;
+				}
+			}
+		}
+		today++;
+	}
+
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	int ret = 0;
+	int idx = 0;
+	int fdump = 0;
+	int fload = 0;
+	int fhelp = 0;
+	int ftest = 0;
+	char key = 0;
+	char *findex = NULL;
+
+	for (idx=1; idx<argc; idx++)
+	{
+		printf("[%d]:<%s>\n", idx, argv[idx]);
+		if (!strcmp(argv[idx], "--dump")) fdump = 1;
+		else if (!strcmp(argv[idx], "--load")) fload = 1;
+		else if (!strcmp(argv[idx], "--test")) ftest = 1;
+		else if (!strcmp(argv[idx], "--file"))
+		{
+			idx++;
+			findex = argv[idx];
+			printf("index: %s\n", findex);
+		}
+		else
+		{
+			printf("usage: %s --file index.txt --dump --load\n", argv[0]);
+			return 0;
+		}
+	}
+
+#if 0
+	if (fdump) printf("fdump\n");
+	if (fload) printf("fload\n");
+	if (findex) printf("%s\n", findex);
+#endif
+
+	//Test mode
+	if (ftest)
+	{
+		printf("Start backtest ...\n");
+		init_mem();
+		mem_load("./stock.mem");
+
+		printf("Press any key to start backtest ...");
+		key = getchar();
+
+		backtest btargv;
+		btargv.beg = 20100101;
+		btargv.end = 20110101;
+		btargv.max_stock = 20;
+		btargv.money = 100 * 10000.00;
+		btargv.taxrate = 0.001;
+
+		back_test_go(&btargv);
+
+		printf("Press any key to release memory.");
+		key = getchar();
+
+		free_mem();
+
+		return 0;
+	}
+
+	init_mem();
+
+	if (findex)
+	{
+		ret = load_index(findex);
+
+		index_file *idxfile = groot.indexfile;
+		for (idx=0; idx<idxfile->used; idx++)
+		{
+			printf("load %s\n", idxfile->name[idx]);
+			ret = load_csv(idxfile->name[idx]);
+		}
+
+		ret = load_csv(idxfile->name[idx]);
+
+		get_tradeday_list();
+	}
+
+	if (fdump) 
+		mem_dump("./stock.mem");
+
+	free_mem();
+	if (fload)
+	{
+		init_mem();
+		mem_load("./stock.mem");
+
+		printf("Press any key to release memory.");
+		key = getchar();
+		free_mem();
+	}
+
+	printf("Press 'x' to exit ... ");
+	while (key != 'x')
+	{
+		key = getchar();
+	}
+
+	printf("\n over \n");
+
+	return ret;
+}
---
+++ stock.h
@@ -0,0 +1,124 @@
+#ifndef __STOCK_H__
+#define __STOCK_H__
+
+#define MAX_STOCKS 						(4000)
+#define MAX_TRADE_DAYS 					(40*365)
+
+typedef struct _tag_section_block
+{
+	char name[32];
+	int  size;
+	int  chk0;
+	int  chk1;
+} section_block;
+
+typedef struct _tag_trade_date
+{
+	int used;
+	int date[MAX_TRADE_DAYS];
+} trade_date;
+
+typedef struct _tag_cci_data
+{
+	float cci14;
+	float cci21;
+	float cci55;
+} cci_data;
+
+typedef struct _tag_mama_data
+{
+	//MA5@,MA10@,MA13@,MA21@,MA34@,MA55@,MA89@,MA144@,MA233@,
+	float ma5;
+	float ma10;
+	float ma13;
+	float ma21;
+	float ma34;
+	float ma55;
+	float ma89;
+	float ma144;
+	float ma233;
+} mama_data;
+
+typedef struct _tag_macd_data
+{
+	//MACD@,DIFF@,DEA@,
+	float macd;
+	float diff;
+	float dea;
+} macd_data;
+
+typedef struct _tag_boll_data
+{
+	//BOLLUPR@,BOLLMID@,BOLLDWN@,
+	float upr;
+	float mid;
+	float dwn;
+} boll_data;
+
+typedef struct _tag_price_data
+{
+	float open;
+	float close;
+	float high;
+	float low;
+} price_data;
+
+typedef struct _tag_day_item
+{
+	int date;
+	price_data price;
+
+	//MA5@,MA10@,MA13@,MA21@,MA34@,MA55@,MA89@,MA144@,MA233@,
+	mama_data  mama;
+
+	//MACD@,DIFF@,DEA@,
+	macd_data  macd;
+
+	//BOLLUPR@,BOLLMID@,BOLLDWN@,
+	boll_data  boll;
+
+	//CCI14@,CCI21@,CCI55@
+	cci_data   cci;
+} day_data;
+
+typedef struct _tag_stock_data
+{
+	void *m_this;
+	char code[16];
+	char name[16];
+	int stockid;
+	int records;
+	int memsize;
+	day_data data[];
+} stock_data;
+
+typedef struct _tag_stock_map
+{
+	int used;
+	stock_data *stocks[MAX_STOCKS];
+} stock_map;
+
+typedef struct _tag_index_data
+{
+	int used;
+	char *buffer;
+	char *name[MAX_STOCKS];
+} index_file;
+
+typedef struct _tag_stock_root
+{
+	index_file *indexfile;
+	trade_date *tradedate;
+	stock_map  *stockmap;
+} mem_root;
+
+typedef struct _tag_backtest_argv
+{
+	int beg;
+	int end;
+	int max_stock;
+	float money;
+	float taxrate;
+} backtest;
+
+#endif /* __STOCK_H__ */
